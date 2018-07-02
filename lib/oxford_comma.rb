5b6dcadfcd521fc4f14128e4d3731f7690fcfac0
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-02 06:19:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
+  
+  array.join(",")
+  
+  case array.size
+  when 1
+    array.join(",")
+  when 2
+    array.join(" and ")
+  else
+    array[-1] = "and #{array[-1]}"
+    array.join(", ")
+    
+  end
 
 end
