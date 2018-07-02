ecdf5b13c33e472ba91534eabe78f42cd20d07f4
Pablo Argueta <pabloargueta27@gmail.com>
2018-07-02 06:13:40 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  
+  array.join(",")
+  
+  case array.size
+  when 1
+    array.join(",")
+  when 2
+    array.join(" and ")
+  when 3
+    array.join(",")
+    
+  end
 
 end
